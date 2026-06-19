e3525e9cd8cb8b1a5096c0e99f2dc9ac9b8f0fa1
V1a5 <vlasmamokiak@gmail.com>
2026-06-19 09:58:17 +0700

    revisi

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -5,8 +5,6 @@ import ReportFlow from "@/src/sections/report-flow";
 export default function Home() {
   return (
     <div className="bg-[var(--background)]">
-      <NavBar />  
-      
       <div className="flex w-full flex-col items-center justify-center py-24 px-16 bg-[var(--background)]">
         <ReportFlow />
         <FormLapor />
